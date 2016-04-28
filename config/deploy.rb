b6d7d75b24f2f6ee44701cb6276ad6a02745bd3a
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-04-28 13:14:55 +0100

    Added deploy

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+
+set :application, 'MadingleyModel'
+set :repo_url, 'git@github.com:unepwcmc/MadingleyModel.git'
+set :branch, 'master'
+
+set :deploy_user, 'wcmc'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+
+set :scm, :git
+set :scm_username, "unepwcmc-read"
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.2.3'
+
+
+set :pty, true
+
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+
+#set :linked_files, %w{config/database.yml db/production.sqlite3}
+
+#set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+
+set :keep_releases, 5
